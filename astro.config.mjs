2fd469eac919107482433afbcc13a9b4dfe6e49e
Chandra Abdul Fattah <chandrashibezzo@gmail.com>
2025-09-01 09:20:01 +0700

    Fix mobile navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://chandrabezzo.github.io/ldpdigital',
+    base: '/ldpdigital', // must match your repo name, with a leading slash
+});
